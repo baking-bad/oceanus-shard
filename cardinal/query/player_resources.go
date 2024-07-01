5a954cf5a733450f28e4ee58bfb1695f78efdcc5
Konst <johnysalvesen@gmail.com>
2024-07-01 20:02:09 +0300

    Added farming key to player resources query

--- cardinal/query/player_resources.go
+++ cardinal/query/player_resources.go
@@ -15,17 +15,41 @@ type PlayerResourcesRequest struct {
 type PlayerResourcesResponse struct {
 	Resources []comp.Resource `json:"resources"`
 	Effects   []comp.Effect   `json:"effects"`
+	Farming   []comp.Farming  `json:"farming"`
 }
 
 func PlayerResources(world cardinal.WorldContext, req *PlayerResourcesRequest) (*PlayerResourcesResponse, error) {
-	_, playerResources, err := system.QueryComponent[comp.PlayerResources](
+	_, playerResources, _ := system.QueryComponent[comp.PlayerResources](
 		world,
 		req.Nickname,
 		filter.Component[comp.Player](),
 		filter.Component[comp.PlayerResources](),
 	)
+
+	_, farmingComponents, err := system.QueryAllComponents[comp.Farming](
+		world,
+		req.Nickname,
+		filter.Component[comp.Building](),
+		filter.Component[comp.Farming](),
+	)
+
+	aggregatedFarmingMap := make(map[comp.ResourceType]float32)
+	for _, farming := range farmingComponents {
+		aggregatedFarmingMap[farming.Type] += farming.Speed
+	}
+
+	aggregatedfarmingSlice := make([]comp.Farming, 0, len(aggregatedFarmingMap))
+
+	for resourceType, speed := range aggregatedFarmingMap {
+		aggregatedfarmingSlice = append(aggregatedfarmingSlice, comp.Farming{
+			Type:  resourceType,
+			Speed: speed,
+		})
+	}
+
 	return &PlayerResourcesResponse{
 		Resources: playerResources.Resources,
 		Effects:   playerResources.Effects,
+		Farming:   aggregatedfarmingSlice,
 	}, err
 }
