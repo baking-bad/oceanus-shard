ecfb35d024d64076db0a7691762b15cdf9857e0d
Konstantin <johnysalvesen@gmail.com>
2024-07-01 14:14:01 +0300

    Updated cardinal to v1.5.1 (#6)
    * Updated cardinal to v1.5.1
    
    * Removed GH build workflow

--- cardinal/component/player_resources.go
+++ cardinal/component/player_resources.go
@@ -3,6 +3,7 @@ package component
 type PlayerResources struct {
 	Resources []Resource `json:"resources"`
 	Effects   []Effect   `json:"effects"`
+	// Farming   []Farming  `json:"farming"`
 }
 
 func (PlayerResources) Name() string {
