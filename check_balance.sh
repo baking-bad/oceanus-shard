a9baf2753872c3d7fd9f5f35c5c4e4df1963bfb2
Konst <johnysalvesen@gmail.com>
2024-09-18 16:13:47 +0300

    Updates for start in celenium and celenium balance checker

---
+++ check_balance.sh
@@ -0,0 +1,17 @@
+balance_url="https://api-arabica.celenium.io/v1/address/$ADDRESS"
+faucet_url="https://faucet.celestia-arabica-11.com/api/v1/faucet/give_me"
+
+while true; do
+    response=$(curl -s $balance_url)
+    spendable=$(echo "$response" | jq -r '.balance.spendable')
+    if [ "$spendable" -lt "$THRESHOLD" ]; then
+        echo "$(date '+%Y-%m-%d %H:%M:%S') Low balance: $spendable < $THRESHOLD. Requesting TIA from faucet."
+        curl -sS -X POST $faucet_url \
+            -H "Content-Type: application/json" \
+            -d "{\"address\":\"$ADDRESS\",\"chainId\":\"$CHAIN_ID\"}"
+    else
+        echo "$(date '+%Y-%m-%d %H:%M:%S') Enough balance: $spendable"
+    fi
+
+    sleep $INTERVAL
+done
